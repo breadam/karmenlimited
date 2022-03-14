d623437a8dfb5686538fe32c52cdeb0aa7f6deae
Cagatay Yaman <cagatay@breadam.com>
2022-03-14 22:34:37 +0300

    basic structure

---
+++ src/components/KarmenSection.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img :src="image"/>
+        <div class="text">
+            asdasdasdasdasdsd
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'KarmenSection',
+    props:["image"],
+}
+</script>
+<style scoped>
+img{
+    width:196px;
+}
+.text{
+    margin-top: 16px;
+    text-align:center;
+}
+</style>
