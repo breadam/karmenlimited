c0c0ee3e097ed0cfc38616d5a48ac5cac6ac7061
Cagatay Yaman <cagatay@breadam.com>
2022-03-15 03:30:54 +0300

    Karmen Sections

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
+  <div class="karmen-nav">
+    <div class="section left">
+    </div>
+    <div class="section right">
+      <span>
+        <select class="karmen-lang">
+          <option>Türkçe</option>
+          <option>English</option>
+        </select>
+      </span>
+    </div>
+  </div>
+
   <div class="karmen-content">
     <div class="karmen-col">
-      <KarmenSection :image="'/assets/guidancy-logo.png'"/>
+      <KarmenSection
+        link="http://guidancyeventsolutions.com" 
+        image="/assets/guidancy-logo.png"
+        title="Guidanc'y"
+        subtitle="Event Solutions & Consultancy"
+      />
     </div>
     <div class="karmen-col">
-      <KarmenSection :image="'/assets/gingeristanbul-logo.png'"/>
+      <KarmenSection
+        link="http://gingeristanbul.com"
+        image="/assets/gingeristanbul-logo.png"
+        title="Ginger"
+        subtitle="Patisserie & Catering"
+      />
     </div>
   </div>
 </template>
@@ -25,7 +48,19 @@ export default {
 
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap');
 
+body,html{
+  background-color:rgb(248,247,239);
+  height:100%;
+  margin:0;
+  padding:0;
+}
+
+#app{
+  height:100%;
+}
+
 .karmen-content{
+  height:100%;
   display:flex;
   justify-content: center;
   font-family: 'Raleway', sans-serif;
@@ -33,7 +68,34 @@ export default {
 
 .karmen-col{
   display:flex;
+  align-self: center;
   margin:32px;
 }
 
+.karmen-nav{
+  position:fixed;
+  left:0;
+  right:0;
+  /*background-color:rgba(228,227,219,0.9);*/
+  padding:6px 64px;
+}
+
+.karmen-nav .section{
+}
+
+.karmen-nav .section.left{
+  float:left;
+}
+
+.karmen-nav .section.right{
+  float:right;
+}
+
+.karmen-lang{
+  text-align:left;
+  padding:6px 12px;
+  border:none;
+  border-radius:4px;
+  margin:0px;
+}
 </style>
--- src/components/KarmenSection.vue
+++ src/components/KarmenSection.vue
@@ -1,23 +1,56 @@
 <template>
-    <div>
-        <img :src="image"/>
-        <div class="text">
-            asdasdasdasdasdsd
+    <a :href="link">
+        <div class="section">
+            <img :src="image"/>
+            <h2 class="title">
+                {{title}}
+            </h2>
+            <h5 class="subtitle">
+                {{subtitle}}
+            </h5>
         </div>
-    </div>
+    </a>
 </template>
 <script>
 export default {
     name:'KarmenSection',
-    props:["image"],
+    props:['link','image','title','subtitle'],
 }
 </script>
 <style scoped>
+
+a,a:visited,a:hover, a:focus, a:active{
+    text-decoration: none;
+    color:inherit;
+}
+
+.section{
+    text-align:center;
+    max-width:256px;
+    padding:48px;
+    background-color:rgb(248,247,239);
+    border-radius:4px;
+    transition:1s all;
+}
+
+.section:hover{
+    box-shadow:0px 2px 4px #333;
+    transform: translate(0px,-4px);
+}
+
+.title{
+    color:#333;
+    margin:0px;
+}
+
+.subtitle{
+    color:#555;
+    margin-top:2px;
+}
+
 img{
     width:196px;
+    margin-bottom: 32px;
 }
-.text{
-    margin-top: 16px;
-    text-align:center;
-}
+
 </style>
